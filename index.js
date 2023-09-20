cf2e1450b40deb89ef43b677de80592ceecce1f3
joshua chivile <joshuachivile@gmail.com>
2023-09-20 20:27:14 +0300

    arithmetic equations

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 16;
+let num4 = 6;
+
+let mod = num3 % num4;
+ 
+let max = Math.max(20,1,2)
